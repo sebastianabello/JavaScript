73dbfcdab96b44bae7b9bdb068be54b8b0a5332f
sebas <108425850+sebastianabello@users.noreply.github.com>
2023-10-11 21:47:48 -0500

    Primeros pasos en Javascript

---
+++ JS/numeros.js
@@ -0,0 +1,21 @@
+
+//Tipos de datos
+
+let a = 2;
+let b = new Number(4);
+console.log(a,b);
+
+//Metodo toFixed
+//Rendonde segun el valor de desimales que tengamos, si el decimal es mayor a #.15 se redonde da 5.2, si es menor el decimal se rendonde a 5.1, depende tambien del cuantos parametros le pasemos
+let c = 7.15;
+console.log(c.toFixed(3));
+console.log(c.toFixed(5));
+console.log(c.toFixed(1));
+
+//Procesamiento en formularios
+//Metodo para formaterar una cadena de texto a Numeros
+let d = "24"; //String
+console.log(d); //Imprime un string}
+console.log(typeof c, typeof d); //muestra que tipo de dato es
+console.log(parseInt(d));
+
