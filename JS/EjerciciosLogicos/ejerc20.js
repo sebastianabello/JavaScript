840b5a4f576be3a87264d32120d59bc9d382cea0
sebas <108425850+sebastianabello@users.noreply.github.com>
2023-10-13 14:33:46 -0500

    Ejercicios de validacion

--- JS/EjerciciosLogicos/ejerc20.js
+++ JS/EjerciciosLogicos/ejerc20.js
@@ -1,1 +1,25 @@
-//20) Programa una función que valide que un texto sea un email válido, pe. miFuncion("jonmircha@gmail.com") devolverá verdadero.
+//20) Programa una función que valide que un texto sea un email válido, pe. miFuncion("juanseabello2004@gmail.com") devolverá verdadero.
+
+const validarEmail = (email = "") => {
+    if(!email) return console.warn("No ingresaste un email");
+    if(typeof email !== "string") return console.error(`El valor "${email}" ingresado, no es una cadena de texto`);
+
+    // ^ --> indica que no puede haber nada antes de la expresion a evaluar.
+    // $ --> indica que no puede haber nada despues de la expresion a evaluar.
+    // + ---> para que lo evalue por cada caracter en la cadena de texto
+
+    let expReg = /^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/i.test(email);
+
+    ///^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/
+
+    ///[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})/i
+
+    return (expReg)
+    ? console.info(`"${email}", Email valido`)
+    : console.warn(`"${email}", Email invalido`)
+}
+
+validarEmail();
+validarEmail(34);
+validarEmail("juan,sas,2fas@gmal.com");
+validarEmail("Juanse.abello.2004@gmail.com");
