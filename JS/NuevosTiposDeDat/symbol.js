1b52cdd21a7eb62692a966c424dc4dfcf4f8fc08
sebas <108425850+sebastianabello@users.noreply.github.com>
2023-10-14 02:30:55 -0500

    Set

--- JS/NuevosTiposDeDat/symbol.js
+++ JS/NuevosTiposDeDat/symbol.js
@@ -9,4 +9,14 @@ let id3 = Symbol("Hola");
 let id4 = Symbol("Hola");
 console.log(id3 === id4);// false
 
+const NOMBRE = Symbol();
 
+const persona = {
+    [NOMBRE]: "Sebastian"
+};
+console.log(persona);
+
+persona.NOMBRE = "Juan sebastian"
+console.log(persona);
+console.log(persona.NOMBRE);
+console.log(persona[NOMBRE]);
