263dad37cd5530c1230d3ef1d298fb4ef0e9f8ed
sebas <108425850+sebastianabello@users.noreply.github.com>
2023-10-19 14:02:29 -0500

    prettier

--- Curso1/Fundamentos/numeros.js
+++ Curso1/Fundamentos/numeros.js
@@ -1,9 +1,8 @@
-
 //Tipos de datos
 
 let a = 2;
 let b = new Number(4); //ya no se usa asi, pero es una forma
-console.log(a,b);
+console.log(a, b);
 
 //Metodo toFixed
 //Rendonde segun el valor de desimales que tengamos, si el decimal es mayor a #.15 se redonde da 5.2, si es menor el decimal se rendonde a 5.1, depende tambien del cuantos parametros le pasemos
@@ -23,4 +22,3 @@ console.log(a + b);
 console.log(a + d); //aqui nos damos cuenta que se opera es con un string es decir se concatenan mas no se suman, tener presente los timpos de datos.
 console.log(c + parseInt(d)); //Enteros
 console.log(c + parseFloat(d)); //Flotantes
-
